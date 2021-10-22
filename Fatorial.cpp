e6f63fbcdf6202edcffc563d31f4712bf5342c48
Tiago Silva Santos <86693308+TiagoSilva777@users.noreply.github.com>
2021-10-22 18:51:41 -0300

    Update Fatorial.cpp

--- Fatorial.cpp
+++ Fatorial.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	static int var=1;//Variaável que retem o valor quando a função é chamada
-	long double fat=1, val, i;
-	cout<<"Digite um valor para saber o fatorial"<<endl;
-	cin>>val;
-	for(i=1; i<=val;i++){
-		fat=fat*i;
+	static int var = 1; //VariÃ¡vel que retÃ©m o valor quando a funÃ§Ã£o Ã© chamada
+	long double fat = 1, val, i;
+	cout << "Digite um valor para saber o fatorial" << endl;
+	cin >> val;
+	for(i = 1; i <= val; i++){
+		fat = fat * i;
 	}
-	cout<<fat<<endl;
-	cout.precision(0);//casas a ser exibidas apos a virgula
-	cout<<fixed<<fat;//exibe valor fixo
+	cout <<fat <<endl;
+	cout.precision(0); //casas a serem exibidas apos a virgula
+	cout<< fixed <<fat; //exibe valor fixo
 	return 0;
 }
